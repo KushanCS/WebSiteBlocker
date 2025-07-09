cc6f8633acea55a3d1940e75eb7da3f4d6b65420
KushanCS <kushan.s.chamikara@gmail.com>
2025-07-09 12:50:41 +0530

    Initial working codes

---
+++ rules.js
@@ -0,0 +1,26 @@
+[
+  {
+    "id": 1,
+    "priority": 1,
+    "action": {
+      "type": "redirect",
+      "redirect": { "extensionPath": "/redirect.html" }
+    },
+    "condition": {
+      "urlFilter": "pornhub.com",
+      "resourceTypes": ["main_frame"]
+    }
+  },
+  {
+    "id": 2,
+    "priority": 1,
+    "action": {
+      "type": "redirect",
+      "redirect": { "extensionPath": "/redirect.html" }
+    },
+    "condition": {
+      "urlFilter": "xvideos.com",
+      "resourceTypes": ["main_frame"]
+    }
+  }
+]
