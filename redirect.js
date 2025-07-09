e012897cfb2e42e674e1bf52a7bb54f6f74eae2f
KushanCS <117761649+KushanCS@users.noreply.github.com>
2025-07-09 12:53:52 +0530

    Delete redirect.js

--- redirect.js
+++
@@ -1,6 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const urlMsg = urlParams.get('msg');
-chrome.storage.sync.get("customMessage", (data) => {
-  const customMsg = urlMsg || data.customMessage || "Default message";
-  document.getElementById("message").innerText = decodeURIComponent(customMsg);
-});
